8213f505af1bc31b5acd790d3c90ba4c80716c7b
Jeremiah <jeremiahiasngu@gmail.com>
2024-08-31 00:20:23 +0100

    changes made to resolve wrong page errors

--- src/Components/InvalidPage/InvalidPage.jsx
+++ src/Components/InvalidPage/InvalidPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "./invalidPage.css"
 
 const InvalidPage = () => {
   return (
     <div className='invalid-page-container'>
-        <h1>This Page Doesn't exist</h1>
+        <h1>Page Not Found - 404</h1>
+        <p>Either this page has been removed or doesn't exist</p>
+        <Link to={"/home"}>Return to Home Page</Link>
     </div>
   )
 }
