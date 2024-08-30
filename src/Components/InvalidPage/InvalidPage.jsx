fdf627d5484602e22d29ac79bab6501f7075923e
Jeremiah <jeremiahiasngu@gmail.com>
2024-08-31 00:23:23 +0100

    changes made to resolve wrong page errors

--- src/Components/InvalidPage/InvalidPage.jsx
+++ src/Components/InvalidPage/InvalidPage.jsx
@@ -7,7 +7,7 @@ const InvalidPage = () => {
     <div className='invalid-page-container'>
         <h1>Page Not Found - 404</h1>
         <p>Either this page has been removed or doesn't exist</p>
-        <Link to={"/home"}>Return to Home Page</Link>
+        <Link to={"/home"} className='link'>Return to Home Page</Link>
     </div>
   )
 }
